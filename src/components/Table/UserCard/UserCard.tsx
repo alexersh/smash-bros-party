9a843d43d0e3e741fba0f77801f0bd11c207874c
Alexey Ershov <alexey.ershov@dataart.com>
2022-12-23 18:47:02 +0100

    added win popup

--- src/components/Table/UserCard/UserCard.tsx
+++ src/components/Table/UserCard/UserCard.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { useLayoutEffect, useState } from 'react';
 import { IUser } from '../../StoreProvider/StoreProvider';
 import styles from './UserCards.module.scss';
 
@@ -7,11 +8,28 @@ interface IUserCard {
 }
 
 const UserCard: React.FC<IUserCard> = ({ user }) => {
+  const [show, setShow] = useState(false);
+
+  useLayoutEffect(() => {
+    if (user.score === 21) {
+      setShow(true);
+
+      setTimeout(() => setShow(false), 10000);
+    }
+  }, [user]);
+
   return (
     <div className={styles.container}>
       <span className={styles.name}>{user.name}</span>
       <div className={`${styles.user} ${styles[user.character.split(' ').join('')]}`}></div>
       <span className={styles.score}>{user.score}</span>
+
+      {show && (
+        <div className={styles.fanfares}>
+          <div>{user.name}, поздравляем! </div>
+          <div>Теперь ТЫ пьяненький!</div>
+        </div>
+      )}
     </div>
   );
 };
